44f6a1fba491784a7658e3a690e6eb41fae6fdde
Sanowarsunny <sanowarsunny1998@gmail.com>
2023-11-19 01:13:58 +0600

    assignment 8

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    //
+    public function index(Request $request, int $id = null){
+        
+        $name = "Donal Trump";
+        $age = "75";
+
+        
+        $data = [
+            "id" => $id,
+            "name"=> $name,
+            "age"=> $age
+        ];
+
+
+        $name = "access_token";
+
+        $value = "123-XYZ";
+
+        $minutes = 60;
+
+        $path = "/";
+
+        $domain = $_SERVER['SERVER_NAME'];
+
+        $secure = false;
+
+        $httpOnly = true;
+
+        return response($data,200)->cookie(
+            $name, $value, $minutes, $path, $domain, $secure, $httpOnly
+        );
+
+    }
+}
