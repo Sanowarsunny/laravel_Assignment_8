70bd88e8146e6544d9dbf2313b58d8be7e99e905
Sanowarsunny <sanowarsunny1998@gmail.com>
2023-11-19 01:33:03 +0600

    modify web php

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Route::get('/profile/{id}', [ProfileController::class,"index"]);
 
 
